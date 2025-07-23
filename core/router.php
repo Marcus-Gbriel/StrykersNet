e67d3c738f0b672945a87ecbe862dd492b85bd05
Marcus-Gbriel <marcusgx45@gmail.com>
2025-07-22 23:34:47 -0300

    adiciona classe router com gerenciamento de rotas e tratamento de requisições

---
+++ core/router.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * 
+ * Roteador de requisições
+ * 
+ * @author Marcus Gabriel Xavier Geraldino <marcusgx45@gmail.com>
+ * @version 0.0.1
+ * 
+ */
+class router
+{
+    private $core = null;
+
+    /**
+     * 
+     * Rotas do sistema
+     * 
+     * @var array
+     * 
+     */
+    private static $routes = [
+        'GET' => [
+            'home' => 'PageController@home',
+            'about' => 'PageController@about',
+            'contact' => 'PageController@contact',
+            'public' => 'PublicController@index',
+        ],
+        'POST' => [],
+        'PUT' => [],
+        'DELETE' => [],
+    ];
+
+    /**
+     * 
+     * Contrutor do roteador
+     * 
+     * @return void
+     * 
+     */
+    public function __construct($core)
+    {
+        $this->core = $core;
+        $this->init();
+    }
+
+    /**
+     * 
+     * Inicializa o roteador
+     * 
+     * @return void
+     * 
+     */
+    private function init(): void
+    {
+        $route = $this->get_request_uri(0);
+        $method = $this->get_method_uri();
+
+        $this->handle_request($method, $route);
+    }
+
+    /**
+     * 
+     * Método para lidar com a requisição
+     *
+     * @param string $method Método HTTP
+     * @param string $route Rota da requisição
+     * @return void
+     * 
+     */
+    private function handle_request(string $method, string $route): void
+    {
+        if (!isset(self::$routes[$method][$route])) {
+            $this->core->error_in_execution(404);
+            return;
+        }
+
+        $controller_name = $this->extract_controller($method, $route);
+        if (!class_exists($controller_name[0])) {
+            $this->core->error_in_execution(404);
+            return;
+        }
+
+        $controller = new $controller_name[0]($this->core, $this);
+        $controller->{$controller_name[1]}();
+    }
+
+    /**
+     * 
+     * Obtém o URI da requisição
+     *
+     * @param integer $level Seleciona o nível
+     * @return string
+     * 
+     */
+    public function get_request_uri(int $level): string
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+        $parts = explode('/', trim($uri, '/'));
+        return $parts[$level] ?? '';
+    }
+
+    /**
+     * 
+     * Obtém o método HTTP da requisição
+     * 
+     * @return string
+     * 
+     */
+    public function get_method_uri(): string
+    {
+        return $_SERVER['REQUEST_METHOD'] ?? 'GET';
+    }
+
+    /**
+     * 
+     * Extrai o controlador da rota
+     * 
+     * @param string $route Rota da requisição
+     * @param string $method Método HTTP
+     * @return array Controlador e método
+     * 
+     */
+    private function extract_controller(string $method, string $route): array
+    {
+        $action = self::$routes[$method][$route];
+        return explode('@', $action) ?? [];
+    }
+}
