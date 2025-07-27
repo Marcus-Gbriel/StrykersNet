f4c2694ccf950677b0ebcf8f7973eebc0deb1d24
Marcus-Gbriel <marcusgx45@gmail.com>
2025-07-27 20:16:31 -0300

    reorganiza a ordem das folhas de estilo no cabeçalho e remove código comentado

--- sources/components/header.php
+++ sources/components/header.php
@@ -4,21 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/css/template.css">
     <link rel="shortcut icon" href="/public/imgs/logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="/public/css/pages.css">
+    <title id="title_system">Strykes - StarCitizen</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="/public/css/pages.css">
+    <link rel="stylesheet" href="/public/css/template.css">
     <script src="/public/js/functions.js"></script>
     <script>
         const router_class = new router();
     </script>
-    <title id="title_system">Strykes - StarCitizen</title>
 </head>
-<!--
-style="position: fixed; top: 0; left: 0; right: 0;"
--->
 
 <body id="body_system">
     <header class="p-3">
